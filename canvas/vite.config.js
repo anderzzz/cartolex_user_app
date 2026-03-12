8c3cc35f9178de7d6c8a504a011d407f43a711db
anderzzz <ohrn.anders@gmail.com>
2026-03-12 12:33:24 +0100

    Fixes to javascript to get production string

--- canvas/vite.config.js
+++ canvas/vite.config.js
@@ -4,6 +4,9 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production')
+  },
   build: {
     outDir: '../cartolex_user_app/static/canvas',
     emptyOutDir: true,
